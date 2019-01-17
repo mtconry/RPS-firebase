a0723d64adbd3148aca1bc78ad369d3482a2e87f
Mike Conry <mikeconry@Mikes-MacBook-Pro.local>
2019-01-17 10:32:19 -0500

    index.html

---
+++ assets/logic.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDW10JDvahYzg9RMiLIclFotpCb0VCo34U",
+    authDomain: "rps-firebase-9f6a5.firebaseapp.com",
+    databaseURL: "https://rps-firebase-9f6a5.firebaseio.com",
+    projectId: "rps-firebase-9f6a5",
+    storageBucket: "rps-firebase-9f6a5.appspot.com",
+    messagingSenderId: "861432141558"
+  };
+  firebase.initializeApp(config);
